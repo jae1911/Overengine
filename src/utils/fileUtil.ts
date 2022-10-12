e907b55358915d54b2213571e166fda76f43e057
Jae Lo Presti (DN0) <git@jae.fi>
2022-10-12 18:14:23 +0300

    utils/fileUtil: add basic directory scour

---
+++ src/utils/fileUtil.ts
@@ -0,0 +1,23 @@
+import { readdirSync, statSync } from 'fs';
+
+import { BASE_CONTENT_DIR } from '../environment';
+
+// Scours directories and only returns markdown files
+function scourDirectory(path: string, _files?: string[]): string[] {
+    let files_: string[] = _files ?? [];
+
+    let files = readdirSync(path);
+    for (let i in files) {
+        let name = path + '/' + files[i];
+        if (statSync(name).isDirectory()) {
+            scourDirectory(name, files_)
+        } else {
+            if (name.split('.').pop() == 'md')
+                files_.push(name.replace(BASE_CONTENT_DIR, ''));
+        }
+    }
+
+    return files_;
+}
+
+export default scourDirectory;
