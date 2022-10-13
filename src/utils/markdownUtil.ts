1271753a2ac28ba263f5181802c4e6eca9574050
Jae Lo Presti (DN0) <git@j4.lc>
2022-10-13 19:08:28 +0300

    utils/markdownUtil: new methods for template generation

--- src/utils/markdownUtil.ts
+++ src/utils/markdownUtil.ts
@@ -1,9 +1,12 @@
 import { existsSync, readFileSync } from 'fs';
 
-import { Marked } from '@ts-stack/markdown';
+import { marked } from 'marked';
 import { parseMarkdownHeaders } from 'markdown-headers';
 
 import { PostMedatada } from '../types/postMetadata';
+import { MenuList } from '../types/menuList';
+import scourDirectory from './fileUtil';
+import { BASE_CONTENT_DIR } from '../environment';
 
 
 const errorMeta: PostMedatada = {
@@ -26,11 +29,11 @@ function pathToParse(path: string): PostMedatada {
     if(path.length < 1 || path.split('.').pop() != 'md' || !existsSync(path))
     {
         res = errorMeta;
-        res.markdown = Marked.parse(errorMeta.markdown);
+        res.markdown = marked.parse(errorMeta.markdown);
     }
     else {
         res = markToParsed(path);
-        res.markdown = Marked.parse(res.markdown);
+        res.markdown = marked.parse(res.markdown);
     }
 
     return res;
@@ -42,13 +45,15 @@ function markToParsed(path: string): PostMedatada {
     let title = noMetaError.title;
     let description = noMetaError.description;
     let date = noMetaError.pubDate;
-
+    let menusList: string[] = [];
 
     if (mdh?.headers) {
         const headers = JSON.parse(JSON.stringify(mdh?.headers));
         title = headers.title ?? noMetaError.title;
         description = headers.description ?? noMetaError.description;
         date = new Date(headers.date) ?? noMetaError.pubDate;
+        if (headers.menus)
+            menusList = headers.menus;
     }
     
     const markdown = mdh?.markdown ?? noMetaError.markdown;
@@ -58,9 +63,112 @@ function markToParsed(path: string): PostMedatada {
         description: description,
         markdown: markdown,
         pubDate: date,
+        menus: menusList,
     };
 
     return res;
 }
 
-export default pathToParse;
+// BaseDomain is request.hostname
+// Protocol is request.protocol
+function generateListFromFile(path: string, baseDomain: string, protocol?: string, onlyIndex?: boolean, noIndex?: boolean, menuName?: string, isBlog?: boolean): MenuList[] {
+    const proto: string = protocol ?? 'https';
+    let res: MenuList[] = [];
+
+    const dirContent: string[] = scourDirectory(path);
+    dirContent.forEach((entry) => {
+        const postMeta: PostMedatada = markToParsed(BASE_CONTENT_DIR + entry);
+
+        let listUri: string = '';
+
+        if (!isBlog) {
+            listUri = `${proto}://${baseDomain}${entry.replace(BASE_CONTENT_DIR, '').replace('.md', '').replaceAll(' ', '-')}`;
+
+            // Take into account the subdir index
+            if (listUri.substring(listUri.lastIndexOf("/")).replace("/", "") == '_index')
+                listUri = listUri.replace('_index', '');
+        } else if (isBlog) {
+            const title = postMeta.title ?? 'No title';
+            listUri = `${proto}://${baseDomain}/${postMeta.pubDate.getFullYear()}/${String(postMeta.pubDate.getMonth() + 1).padStart(2, '0')}/${String(postMeta.pubDate.getDay() + 1).padStart(2, '0')}/${title.replaceAll(' ', '-').toLowerCase()}/`;
+        }
+
+        const menuEntry: MenuList = {
+            title: postMeta.title as string,
+            date: postMeta.pubDate,
+            link: listUri,
+        };
+
+        if (menuName && postMeta.menus && postMeta.menus.length > 0){
+            if (postMeta.menus.includes(menuName)) {
+                res.push(menuEntry);
+            }
+        } else if (!menuName) {
+            if (onlyIndex && entry.includes('_index.md'))
+                res.push(menuEntry);
+            else if (!onlyIndex && !noIndex)
+                res.push(menuEntry);
+            else if (!onlyIndex && noIndex && !entry.includes('_index.md'))
+                res.push(menuEntry);
+        }
+    });
+
+    return res;
+}
+
+function listToMarkdown(path: string, baseDomain: string, protocol?: string, onlyIndex?: boolean, noIndex?: boolean, showDate?: boolean, menuName?: string, isBlog?: boolean): string {
+    let res: string = '';
+
+    let fileList: MenuList[] = generateListFromFile(path, baseDomain, protocol, onlyIndex, noIndex, menuName, isBlog);
+
+    fileList = fileList.sort(
+        (objA, objB) => objB.date.getTime() - objA.date.getTime(),
+    );
+
+    fileList.forEach((page) => {
+        if (showDate){
+            const date = `${page.date.getFullYear()}/${String(page.date.getMonth() + 1).padStart(2, '0')}/${String(page.date.getDay() + 1).padStart(2, '0')}`;
+            res += ` - ${date} [${page.title}](${page.link})\n`;
+        }
+        else
+            res += ` - [${page.title}](${page.link})\n`;
+    });
+
+    return res;
+}
+
+function generateList(path: string, baseDomain: string, protocol?: string): string {
+    let res: string = '';
+
+    res = marked.parse(listToMarkdown(path.replace(/.$/, ''), baseDomain, protocol, false, true));
+
+    return res;
+}
+
+function generatePageMenu(baseDomain: string, protocol?: string): string {
+    let res: string = '';
+
+    const listMd = listToMarkdown(BASE_CONTENT_DIR, baseDomain, protocol, false, false, false, 'main');
+    res = marked.parse(listMd);
+
+    return res;
+}
+
+function generateWikiMenu(baseDomain: string, protocol?: string): string {
+    let res: string = '';
+
+    const listMd = listToMarkdown(BASE_CONTENT_DIR, baseDomain, protocol, true, false, false, 'wiki');
+    res = marked.parse(listMd);
+
+    return res;
+}
+
+function generateBlogList(baseDomain: string, protocol?: string): string {
+    let res: string = '';
+
+    res = marked.parse(listToMarkdown(BASE_CONTENT_DIR + '/blog', baseDomain, protocol, false, true, true, undefined, true));
+
+
+    return res;
+}
+
+export { pathToParse, generateListFromFile, listToMarkdown, generateList, generatePageMenu, generateWikiMenu, generateBlogList };
