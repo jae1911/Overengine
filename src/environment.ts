fe9dfcce134ff8871f8ddee3bcd2ffa306e2d9c6
Jae Lo Presti (DN0) <git@jae.fi>
2022-10-13 09:24:49 +0300

    environment: use dirname instead of hardcode

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 import { env } from 'process';
 import dotenv from 'dotenv';
+import { join } from 'path';
 
 dotenv.config();
 
-export const BASE_CONTENT_DIR: string = env.CONTENT_DIR ?? '../content';
+export const BASE_CONTENT_DIR: string = env.CONTENT_DIR ?? join(__dirname, '../content');
