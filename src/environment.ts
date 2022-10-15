07df328ddf2d6863da38481f0c481bd06c120cfc
Jae Lo Presti (DN0) <git@j4.lc>
2022-10-15 11:40:47 +0300

    META: add remote repo for content

--- src/environment.ts
+++ src/environment.ts
@@ -4,5 +4,5 @@ import { join } from 'path';
 
 dotenv.config();
 
-export const BASE_CONTENT_DIR: string = env.CONTENT_DIR ?? join(__dirname, '../content');
+export const BASE_CONTENT_DIR: string = env.CONTENT_DIR ?? join(__dirname, '../content/content');
 export const SITE_NAME: string = env.SITE_NAME ?? 'Jae\'s Website';
