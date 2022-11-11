fb8360f2d0d4bfa9ae67d95c1e66fe94635dacc6
Jae Lo Presti (DN0) <git@j4.lc>
2022-11-11 17:08:54 +0200

    routes/redirect: refactor for linting

--- src/routes/redirects.ts
+++ src/routes/redirects.ts
@@ -1,69 +1,70 @@
 import { FastifyPluginCallback, FastifyRequest } from 'fastify';
+
 import MatrixUtils from '../utils/matrixUtils';
 
 const matrixUtil = new MatrixUtils();
 
 const plugin: FastifyPluginCallback = function (fastify, opts, next): void {
-    fastify.get("/redir/tw", (request, reply): void => {
-        reply.redirect('https://futurice.com/tech-weeklies');
+    fastify.get("/redir/tw", async (request, reply): void => {
+        await reply.redirect('https://futurice.com/tech-weeklies');
     });
 
-    fastify.get("/redir/cv", (request, reply): void => {
-        reply.redirect('https://git.jae.fi/jae');
+    fastify.get("/redir/cv", async (request, reply): void => {
+        await reply.redirect('https://git.jae.fi/jae');
     });
 
-    fastify.get('/redir/metacode', (request, reply) => {
-        reply.redirect('https://git.jae.fi/jae/overengine');
+    fastify.get('/redir/metacode', async (request, reply) => {
+        await reply.redirect('https://git.jae.fi/jae/overengine');
     });
 
-    fastify.get('/redir/gh', (request, reply) => {
-        reply.redirect('https://github.com/jae1911');
+    fastify.get('/redir/gh', async (request, reply) => {
+        await reply.redirect('https://github.com/jae1911');
     });
 
-    fastify.get('/redir/co', (request, reply) => {
-        reply.redirect('/blog/2021/11/02/moving-forward/');
+    fastify.get('/redir/co', async (request, reply) => {
+        await reply.redirect('/blog/2021/11/02/moving-forward/');
     });
 
-    fastify.get('/redir/matrixcritic', (request, reply) => {
-        reply.redirect('https://www.aminda.eu/matrix/');
+    fastify.get('/redir/matrixcritic', async (request, reply) => {
+        await reply.redirect('https://www.aminda.eu/matrix/');
     });
 
-    fastify.get('/redir/friend1', (request, reply) => {
-        reply.redirect('https://mikaela.eu');
+    fastify.get('/redir/friend1', async (request, reply) => {
+        await reply.redirect('https://mikaela.eu');
     });
 
-    fastify.get('/redir/ghactivity', (request, reply) => {
-        reply.redirect('https://github.com/issues?q=is%3Aopen+author%3Ajae1911+archived%3Afalse');
+    fastify.get('/redir/ghactivity', async (request, reply) => {
+        await reply.redirect('https://github.com/issues?q=is%3Aopen+author%3Ajae1911+archived%3Afalse');
     });
 
-    fastify.get('/redir/srht', (request, reply) => {
-        reply.redirect('https://sr.ht/~jae/');
+    fastify.get('/redir/srht', async (request, reply) => {
+        await reply.redirect('https://sr.ht/~jae/');
     });
 
-    fastify.get('/redir/yt', (request, reply) => {
-        reply.redirect('https://youtube.com/@j4l');
+    fastify.get('/redir/yt', async (request, reply) => {
+        await reply.redirect('https://youtube.com/@j4l');
     });
 
     // Matrix room joins
-    fastify.get('/redir/matrix/:roomid/:server', (request: FastifyRequest<{
-        Params: {
-            roomid: string,
-            server: string,
+    fastify.get('/redir/matrix/:roomid/:server', async (request: FastifyRequest<{
+        readonly Params: {
+            readonly roomid: string,
+            readonly server: string,
         },
     }>, reply) => {
         const { roomid, server } = request.params;
-        reply.redirect(matrixUtil.standardRedirector(roomid, server));
+        await reply.redirect(matrixUtil.standardRedirector(roomid, server));
     });
 
-    fastify.get('/redir/matrix/:roomid/:server/element/:room', (request: FastifyRequest<{
-        Params: {
-            roomid: string,
-            server: string,
-            room?: boolean,
+    fastify.get('/redir/matrix/:roomid/:server/element/:room', async (request: FastifyRequest<{
+        readonly Params: {
+            readonly roomid: string,
+            readonly server: string,
+            readonly room?: boolean,
         },
     }>, reply) => {
         const { roomid, server, room } = request.params;
-        reply.redirect(matrixUtil.elementRedirector(roomid, server, room));
+        await reply.redirect(matrixUtil.elementRedirector(roomid, server, room));
     });
 
     next();
