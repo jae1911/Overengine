bb55635951819f3ef43f154e6dfb629d0b030a29
Jae Lo Presti (DN0) <git@j4.lc>
2022-11-11 17:17:52 +0200

    routes/blog: refactor for linting

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -7,61 +7,87 @@ import { pathToParse, generatePageMenu, generateWikiMenu, generateBlogList, blog
 const plugin: FastifyPluginCallback = function (fastify, opts, next): void {
     // Blog index
     fastify.get("/blog/", async (request, reply) => {
+        const content = await pathToParse(BASE_CONTENT_DIR + "/blog/_index.md");
+        const pagesMenu = generatePageMenu(request.hostname);
+        const wikiMenu = generateWikiMenu(request.hostname);
+        const list = generateBlogList(request.hostname);
+
         await reply.view('/templates/index.ejs', { 
-            content: await pathToParse(BASE_CONTENT_DIR + '/blog/_index.md'),
+            content,
             sitename: SITE_NAME,
-            pagesMenu: generatePageMenu(request.hostname),
-            wikiMenu: generateWikiMenu(request.hostname),
-            list: generateBlogList(request.hostname),
+            pagesMenu,
+            wikiMenu,
+            list,
             domains: DOMAINS_ADVERTISED,
         });
     });
 
     // Blog tag
     fastify.get("/blog/tag/:tag/", async (request: FastifyRequest<{
-        Params: {
-            tag: string,
+        readonly Params: {
+            readonly tag: string,
         },
     }>, reply) => {
+        const { tag } = request.params;
+
+        const content = await pathToParse(BASE_CONTENT_DIR + "/blog/_index.tags.md");
+        const pagesMenu = generatePageMenu(request.hostname);
+        const wikiMenu = generateWikiMenu(request.hostname);
+        const list = generateBlogListTagged(request.hostname, tag);
+
         await reply.view('/templates/index.ejs', {
-            content: await pathToParse(BASE_CONTENT_DIR + '/blog/_index.tags.md'),
+            content,
             sitename: SITE_NAME,
-            pagesMenu: generatePageMenu(request.hostname),
-            wikiMenu: generateWikiMenu(request.hostname),
-            list: generateBlogListTagged(request.hostname, request.params.tag),
+            pagesMenu,
+            wikiMenu,
+            list,
             domains: DOMAINS_ADVERTISED,
         });
     });
 
     // Blog posts
-    fastify.get("/blog/:year/:month/:day/:title/", (request, reply): void => {
+    fastify.get("/blog/:year/:month/:day/:title/", async (request, reply) => {
         const reqUri = request.url;
-        reply.view('/templates/blog.ejs', {
-            content: blogFinder(reqUri),
+
+        const content = blogFinder(reqUri);
+        const pagesMenu = generatePageMenu(request.hostname);
+        const wikiMenu = generateWikiMenu(request.hostname);
+
+        await reply.view('/templates/blog.ejs', {
+            content,
             sitename: SITE_NAME,
-            pagesMenu: generatePageMenu(request.hostname),
-            wikiMenu: generateWikiMenu(request.hostname),
+            pagesMenu,
+            wikiMenu,
             moment: moment,
             domains: DOMAINS_ADVERTISED,
         });
     });
 
     // RSS Feed blog
-    fastify.get("/blog/index.xml", (request, reply): void => {
+    fastify.get("/blog/index.xml", async (request, reply) => {
         const feed = generateFeeds(request.hostname, true);
-        reply.type('application/rss+xml ').send(feed.rss2());
+
+        const generatedFeed = feed.rss2();
+
+        await reply.type('application/rss+xml ').send(generatedFeed);
     });
 
     // JSON Feed blog
-    fastify.get("/blog/index.json", (request, reply): void => {
+    fastify.get("/blog/index.json", async (request, reply) => {
         const feed = generateFeeds(request.hostname, true);
-        reply.type('application/feed+json').send(feed.json1());
+
+        const generatedFeed = feed.json1();
+
+        await reply.type('application/feed+json').send(generatedFeed);
     });
 
     // ATOM Feed blog
-    fastify.get("/blog/index.atom", (request, reply): void => {
+    fastify.get("/blog/index.atom", async (request, reply) => {
         const feed = generateFeeds(request.hostname, true);
-        reply.type('application/atom+xml').send(feed.atom1());
+
+        const generatedFeed = feed.atom1();
+
+        await reply.type('application/atom+xml').send(generatedFeed);
     });
 
     next();
