559d549ab61891b7236dcf0eaa637db83d71fe7f
Jae Lo Presti (DN0) <git@j4.lc>
2022-12-11 10:32:23 +0200

    matrixUtils: cleanup

--- src/utils/matrixUtils.ts
+++ src/utils/matrixUtils.ts
@@ -1,18 +1,5 @@
 const defaultVias: readonly string[] = ['jae.fi'];
 
-/* class MatrixUtils {
-
-    public elementRedirector(roomid: string, server: string, room?: boolean, via?: string[]): string {
-        const viaList = via?.join('&via=') ?? defaultVias.join('&via=');
-        const directorSymbol = room ? '#' : '!';
-
-        const roomUri = `element://vector/webapp/#/room/${directorSymbol}${roomid}:${server}?via=${viaList}`;
-
-        return roomUri;
-    }
-
-} */
-
 const matrixSchemeGenerator = (roomid: string, server: string, via?: readonly string[]): string => {
     const viaList = via?.join("&via=") ?? defaultVias.join("&via=");
     const roomUri = `matrix:roomid/${roomid}:${server}?action=join&via=${viaList}`;
