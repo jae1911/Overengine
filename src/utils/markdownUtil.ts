5f41153941def75666897472de6fd0f9be6e3030
Jae Lo Presti (DN0) <jae.lopresti@futurice.com>
2023-04-19 09:00:58 +0300

    pathToParse: move blog list shortcode to its own function

--- src/utils/markdownUtil.ts
+++ src/utils/markdownUtil.ts
@@ -38,7 +38,7 @@ const mdParser = MarkdownIt({
     });
 
 // PARSE MD AND RETURN IT
-const pathToParse = async (path: string, blog?: boolean, baseDomain?: string): Promise<PostMedatada> => {
+const pathToParse = async (path: string, isBlog?: boolean, baseDomain?: string): Promise<PostMedatada> => {
     if (path.length < 1 || [...path.split(".")].pop() != "md" || !existsSync(path)) {
         const resMeta: PostMedatada = {
             title: notFoundMeta.title,
@@ -57,12 +57,12 @@ const pathToParse = async (path: string, blog?: boolean, baseDomain?: string): P
     const shortCodedMarkdown = await shortCodeOWM(await shortCodeBGP(await shortCodeWakaTime(shortCodeConstruction(parsedMeta.markdown))));
 
     // Launch rendering
-    const gotBlogDomain = blog && baseDomain;
+    const gotBlogDomain = isBlog && baseDomain;
     const response: PostMedatada = {
         date: parsedMeta.date,
         markdown: mdParser.render(
             gotBlogDomain
-                ? shortCodedMarkdown.replaceAll("{{< postlist >}}", listToMarkdown(BASE_CONTENT_DIR + "/blog", baseDomain, false, true, false, undefined, true, 5))
+                ? shortcodeBlogList(shortCodedMarkdown, baseDomain)
                 : shortCodedMarkdown),
         pubDate: parsedMeta.pubDate,
         draft: parsedMeta.draft,
@@ -81,6 +81,10 @@ const pathToParse = async (path: string, blog?: boolean, baseDomain?: string): P
 // Shortcodes
 const shortCodeConstruction = (input: string): string => {
     return input.replaceAll("{{< construction >}}", "<div class=\"construction\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z\"></path></svg><h2>What comes next is a work in progress!</h2></div>")
+}
+
+const shortcodeBlogList = (markdown: string, baseDomain: string): string => {
+    return markdown.replaceAll("{{< postlist >}}", listToMarkdown(BASE_CONTENT_DIR + "/blog", baseDomain, false, true, false, undefined, true, 5));
 }
 
 const shortCodeWakaTime = async (input: string): Promise<string> => {
