560317571c0b366c84a7f0f50dc3ab37528eafe1
Jae Lo Presti [j4] <git@777.tf>
2024-02-11 13:39:29 +0200

    tests: matrix scheme generator tests

---
+++ tests/matrix.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from "@jest/globals";
+
+import { elementSchemeGenerator, matrixSchemeGenerator } from "../src/utils/matrixUtils";
+
+describe("Matrix scheme generator", () => {
+  test("scheme generator room", () => {
+    const sampleScheme = "matrix:roomid/test:example.com?action=join&via=jae.fi";
+
+    expect(matrixSchemeGenerator("test", "example.com")).toBe(sampleScheme);
+  });
+
+  test("scheme generator user", () => {
+    const sampleScheme = "matrix:u/test:example.com?action=join&via=jae.fi";
+
+    expect(matrixSchemeGenerator("test", "example.com", undefined, true)).toBe(
+      sampleScheme,
+    );
+  });
+});
+
+describe("Element scheme generator", () => {
+  test("scheme generator room", () => {
+    const sampleScheme =
+      "element://vector/webapp/#/room/!test:example.com?via=jae.fi";
+
+    expect(elementSchemeGenerator("test", "example.com", false)).toBe(
+      sampleScheme,
+    );
+  });
+
+  test("scheme generator user", () => {
+    const sampleScheme =
+      "element://vector/webapp/#/room/#test:example.com?via=jae.fi";
+
+    expect(elementSchemeGenerator("test", "example.com", true)).toBe(sampleScheme);
+  });
+});
